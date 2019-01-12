bd7e05f9742c99fe8d80e48d125225d9b101eb17
tkent42 <tkent42@gmail.com>
2019-01-12 15:02:08 -0600

    Update runslideshow.sh

--- PiScripts/runslideshow.sh
+++ PiScripts/runslideshow.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+#I use a Samba share from my server to host my images. At some point I would like to update it to a dropbox.
 sudo mount.cifs //NETWORK_LOCATION /home/pi/Art -o user=user,password=password
 sleep 4
 #@export DISPLAY=:0
-feh -Y -x -q -D 200 -B black -F --auto-rotate -Z -z -r /home/pi/Art
+feh -Y -x -q -D 200 -B black -F --auto-rotate -Z -z -r /home/pi/Art/Slideshow
